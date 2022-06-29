d68a8ec7299387e86a7b1f7e90d272f3f4343d82
ignacio lizarazu aramayo <lizarazuaramayoignacio@gmail.com>
2022-06-29 09:07:16 -0400

    comit final

---
+++ src/main/java/ejercicio5/Decorator.java
@@ -0,0 +1,11 @@
+package ejercicio5;
+
+public abstract class Decorator implements ICelular {
+    private ICelular componente;
+    public Decorator(ICelular componente){
+        this.componente = componente;
+    }
+    public void operation(){
+        componente.operation();
+    }
+}
---
+++ src/main/java/ejercicio5/Marcas.java
@@ -0,0 +1,5 @@
+package ejercicio5;
+
+public enum Marcas {
+    Apple, Samsung, Xiaomi
+}
